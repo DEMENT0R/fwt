39571ab64632fc235fd8b8c0ebbf3c443cec0cd0
Maxim Bezrukavnikov <dmntr@rambler.ru>
2024-12-09 16:56:49 +1000

    cars draft

---
+++ database/seeders/CarSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Actions\GetCarsAction;
+use App\Models\Car;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// debug
+Route::get('/cars', [\App\Http\Controllers\CarController::class, 'index']);
+Route::get('/cars/{id}', [\App\Http\Controllers\CarController::class, 'show']);
