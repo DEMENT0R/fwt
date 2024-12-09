84a1ff6be7ed4aa16c3ba8dc9784abbab580cb8c
Maxim Bezrukavnikov <dmntr@rambler.ru>
2024-12-09 16:58:57 +1000

    models routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [\App\Http\Controllers\CarController::class, 'models']);
+Route::get('/models', [\App\Http\Controllers\CarController::class, 'models']);
+Route::get('/models/{model}', [\App\Http\Controllers\CarController::class, 'model']);
+
 // debug
 Route::get('/cars', [\App\Http\Controllers\CarController::class, 'index']);
 Route::get('/cars/{id}', [\App\Http\Controllers\CarController::class, 'show']);
